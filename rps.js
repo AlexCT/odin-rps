efb1013e85f972202f1d6c19acc35c28f2656c8a
AlexCT <tresseltac@gmail.com>
2022-06-12 12:16:29 -0700

    Function outlines

--- rps.js
+++ rps.js
@@ -3,7 +3,61 @@
 
 // Randomly returns "rock", "paper", or "scissors"
 function computerPlay() {
-    return "test";
+    const selections = ["rock", "paper", "scissors"];
+    return selections[Math.floor(Math.random() * selections.length)];
 }
 
-console.log(computerPlay());
+// Given a playerSelection and computerSelection, return the winner
+// as a string.
+// ex. playRound("scissors", "rock") -> "You Lose! Paper beats Scissors"
+function playRound(playerSelection, computerSelection) {
+    let choice = playerSelection.toLowerCase();
+
+    if (choice === computerSelection) {
+        return "Tie! You both chose " + computerSelection;
+    } 
+
+    switch (choice) {
+        case "rock":
+            switch (computerSelection) {
+                case "paper":
+                    return "You Lose! Paper beats Rock";
+                case "scissors":
+                    return "You Win! Rock beats Scissors";
+                default:
+                    return "Error: Computer Selection not found";
+            }
+        case "paper":
+            switch (computerSelection) {
+                case "rock":
+                    return "You Win! Paper beats Rock";
+                case "scissors":
+                    return "You Lose! Scissors beats Paper";
+                default:
+                    return "Error: Computer Selection not found";
+            }
+        case "scissors":
+            switch (computerSelection) {
+                case "paper":
+                    return "You Win! Scissors beats Paper";
+                case "rock":
+                    return "You Lose! Rock beats Scissors";
+                default:
+                    return "Error: Computer Selection not found";
+            }
+        default:
+            return "Error: Player Selection not found";
+    }
+    return "Error: No Moves Found";
+}
+
+// Plays a 5 round game, keeps score and reports winner and loser at the end
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(computerPlay(), computerPlay()));
+    }
+}
+
+game();
+
